1ee8bceca5c5ab823e9689a51595b8cb5ecf07ce
nikhil286 <nikhildude281@gmail.com>
2019-04-20 10:54:30 +0530

    Selection_sort time complexity O(n^2).

---
+++ Selection_sort.cpp
@@ -0,0 +1,26 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,j;
+    int m;
+    int arr[5]={34,1,8,98,0};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    for(i=0;i<n-1;i++)
+    {
+
+        for(j=i+1;j<n;j++)
+        {
+          if(arr[j]<arr[i])
+          {
+              swap(arr[i],arr[j]);
+
+          }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
